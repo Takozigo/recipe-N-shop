b3bec2a38df82bea95fb83d1ff9d6a572b28908a
takozigo <takozigo@gmail.com>
2026-01-12 11:11:05 +0900

    remove nitro (wrong select from template)

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import tailwindcss from '@tailwindcss/vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import { nitro } from 'nitro/vite'
 import { defineConfig } from 'vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import netlify from '@netlify/vite-plugin-tanstack-start'
@@ -10,7 +9,6 @@ import netlify from '@netlify/vite-plugin-tanstack-start'
 const config = defineConfig({
   plugins: [
     devtools(),
-    nitro(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
